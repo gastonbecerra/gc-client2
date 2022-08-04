ce216b170efae49c979c57a29e5c965efc7904a3
gaston becerra <gaston.becerra@gmail.com>
2022-08-04 02:06:05 -0300

    create new rule

--- src/pages/Context/ContextCreate.jsx
+++ src/pages/Context/ContextCreate.jsx
@@ -1,16 +1,27 @@
 import React,{useState, useEffect} from 'react';
-import { SelectType } from '../../components/inputs';
 import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { fetchVars } from '../../store/slices/vars';
 
 export default function ContextCreate() {
-    const [selected, setSelected] = useState(false); 
+    
+    const [selectedVariable, setSelected] = useState(false); 
+    const [rulesOperation, setOperation] = useState(false); 
+
     const [state, setState] = useState({
         name: '',
         description: '',
         rules: [],
-        vars: ''
+        vars: false,        
     })
+
+    const dispatch = useDispatch();
+    
     const { vars } = useSelector(state => state.vars);
+
+    useEffect(()=>{
+        !vars && dispatch(fetchVars());
+        console.log(vars);
+    },[])
 
     const handleChange = (e) => {
         setState({
@@ -28,21 +39,15 @@ export default function ContextCreate() {
         console.log(state);
     }
 
-    const handleSelected = (e) => {
+    const handleSelectedVariable = (e) => {
         setSelected(e.target.value)
-        console.log(selected);
     }
 
-    const displayVarControl = () =>{
-        if(selected){
-            var selected_var = vars.filter(v => v.name === selected)
-        
-        }
-        return (
-            <pre>
-                {JSON.stringify(selected_var, null, 2)}
-            </pre>
-        )
+    const displaySelectedVariable = () =>{ // ves tucu! por cosas como estas camelcase es una verga
+    }
+
+    const handleRulesOperations = (e) => {
+        setOperation(e.target.value);
     }
 
   return (
@@ -57,12 +62,75 @@ export default function ContextCreate() {
             <br></br>
             <h3>reglas</h3>
             
-            - seleccione una variable
-            - ingrese una operacion (identidad, between, mayor, etc)
-            - ingrese un valor
+
             <div>
-            <SelectType input={['vars', {value:[]}]} handleValue={(e)=>handleSelected(e)}/>
+                <label htmlFor='selectedVariable'>Select variable</label>
+                <select id="selectedVariable" name="selectedVariable" onChange={(e) => handleSelectedVariable(e)}>
+                    {
+                        vars && vars.map( (varItem, index) => {
+                            return (
+                                <option key={index} value={varItem.name}>{varItem.name}</option>
+                                )
+                            })
+                        }
+                </select>
             </div>
+
+            {
+                selectedVariable && vars.map( (varItem, index) => {
+                    if(varItem.name === selectedVariable) {
+                        return (
+                            <div key={index} style={{ 
+                                border: 'black solid 0.5px',
+                                marginTop: '10px',
+                                padding: '10px',
+                                width: '60vw',
+                                fontSize: '80%',
+                                textAlign: 'center'
+                            }}>
+                                <h4>variable seleccionada</h4>                    
+                                {varItem.name}<br/>
+                                {varItem.hashtag}<br/>
+                                {varItem.scale}
+
+                                <h4>ingrese una operacion</h4>
+                                    {
+                                        {
+                                            'currency (ARS)': 
+                                                <select id="rulesOperation" name="rulesOperation"
+                                                    onChange={(e) => handleRulesOperations(e)}>>
+                                                    <option value="eq">equal</option>
+                                                    <option value="bt">between</option>
+                                                    <option value="gt">greater than</option>
+                                                    <option value="lt">less than</option>                                                    
+                                                </select>,
+                                            default: 'x'
+                                        }[varItem.scale]
+                                    }
+                                <br/>
+
+                                {
+                                    rulesOperation &&
+                                        <div>
+                                            <h4>ingrese un valor</h4>
+                                            {
+                                                {
+                                                    'eq': <input type="text" id="rulesValue" name="rulesValue" />,
+                                                    // 'bt': <input type="text" id="rulesValue" name="rulesValue" />,
+                                                    'gt': <input type="text" id="rulesValue" name="rulesValue" />,
+                                                    'lt': <input type="text" id="rulesValue" name="rulesValue" />,
+                                                }[rulesOperation]
+                                            }
+                                        </div>
+                                }
+
+                            </div>
+                        )
+                    }
+                })
+                
+            }
+
             <h3>json reglas</h3>
 
             <pre>{state.rules}</pre>
@@ -70,7 +138,6 @@ export default function ContextCreate() {
             <button type="submit" value="Create context"/>
         </form>
 
-            {displayVarControl()}
     </>
   )
 }
